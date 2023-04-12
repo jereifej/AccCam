a65dae0eb0cad86add34d38903754ab12f29ac1a
jereifej <jhe7@pitt.edu>
2023-04-12 12:07:09 -0400

    Rebase Working onto Main

--- CV/FaceRecognition.py
+++ CV/FaceRecognition.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_MSMF)
 # Turn off auto exposure
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
 cap.set(cv2.CAP_PROP_FPS, 30)
@@ -21,14 +21,12 @@ while True:
 
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    #print(isinstance(faces, tuple))
-    #print(isinstance(faces, np.ndarray))
     cv2.imshow('output', frame)
     count += 1
     diff = time.time() - instFPS
     acc += diff
 
-   # print("FPS: ", 1.0 / diff)  # FPS = 1 / time to process loop
+    print("FPS: ", 1.0 / diff)  # FPS = 1 / time to process loop
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
